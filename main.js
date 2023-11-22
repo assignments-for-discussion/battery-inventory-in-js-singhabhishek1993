739ad613b2077130a9a9e73985a8cf9b7881e6d5
singhabhishek1993 <91404947+singhabhishek1993@users.noreply.github.com>
2023-11-22 18:08:09 +0530

    Update main.js
    1. Added a function countBatteriesByHealth to calculate state-of-health (SoH) and classify batteries into healthy, exchange, or failed categories.
    2. Utilized the provided SoH classification criteria.
    3. Updated the test function to use assert.strictEqual for precise comparisons.
    4. Improved comments for code clarity and readability.

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 const assert = require('assert');
 
 function countBatteriesByHealth(presentCapacities) {
+  const ratedCapacity = 120;
+  let healthyCount = 0;
+  let exchangeCount = 0;
+  let failedCount = 0;
+
+  for (const presentCapacity of presentCapacities) {
+    const soh = (presentCapacity / ratedCapacity) * 100;
+
+    if (soh > 80) {
+      healthyCount++;
+    } else if (soh >= 62) {
+      exchangeCount++;
+    } else {
+      failedCount++;
+    }
+  }
+
   return {
-    healthy: 0,
-    exchange: 0,
-    failed: 0
+    healthy: healthyCount,
+    exchange: exchangeCount,
+    failed: failedCount,
   };
 }
 
 function testBucketingByHealth() {
   console.log('Counting batteries by SoH...');
   const presentCapacities = [113, 116, 80, 95, 92, 70];
-  counts = countBatteriesByHealth(presentCapacities);
-  assert(counts["healthy"] == 2);
-  assert(counts["exchange"] == 3);
-  assert(counts["failed"] == 1);
+  const counts = countBatteriesByHealth(presentCapacities);
+  
+  assert.strictEqual(counts.healthy, 2, "Healthy count");
+  assert.strictEqual(counts.exchange, 3, "Exchange count");
+  assert.strictEqual(counts.failed, 1, "Failed count");
+
   console.log("Done counting :)");
 }
 
